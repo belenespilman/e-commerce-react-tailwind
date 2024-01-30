7f0651f836a26fb90618271dcc6ef0139b00557e
belenespilman <belen.espilman@gmail.com>
2024-01-30 17:20:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='bg-red-300'>
+        Hello World
+      </div>
+      
+    </>
+  )
+}
+
+export default App
