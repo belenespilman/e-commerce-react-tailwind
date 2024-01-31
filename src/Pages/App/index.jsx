066409297e6ae2468b345ed0debf8a18089f6aed
belenespilman <belen.espilman@gmail.com>
2024-01-31 14:31:18 +0100

    routing with react-router-dom

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,26 +1,31 @@
-import Home from "../Home"
-import './App.css';
+import {useRoutes, BrowserRouter} from 'react-router-dom'
+import Home from "../Home";
+import MyOrder from '../Home/MyOrder';
 import MyAccount from "../MyAccount";
 import MyOrders from "../MyOrders";
 import NotFound from "../NotFound";
 import SignIn from "../SingIn";
+import './App.css';
 
 
-function App() {
+const AppRoutes= () => {
+  let routes= useRoutes([
+    {path: '/', element: <Home/>},
+    {path: '/my-account', element: <MyAccount/>},
+    {path: '/my-order', element: <MyOrder/>},
+    {path: '/my-orders', element: <MyOrders/>},
+    {path: '/*', element: <NotFound/>},
+    {path: '/sign-in', element: <SignIn/>},
+  ])
+  return routes
+}
 
+const App = () => {
   return (
-    <>
-
-      <div className="font-bold">
-        <Home/>
-        <MyAccount/>
-        <MyOrders/>
-        <NotFound/>
-        <SignIn/>
-        
-      </div>
-      
-    </>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  
   )
 }
 
---
+++ src/Pages/Home/MyOrder/index.jsx
@@ -0,0 +1,14 @@
+function MyOrder() {
+
+    return (
+      <>
+  
+        <div className='bg-red-100'>
+          My order
+        </div>
+        
+      </>
+    )
+  }
+  
+  export default MyOrder
