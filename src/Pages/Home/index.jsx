afca0992aec355ae99ec3d162aa0b21f89c0f565
belenespilman <belen.espilman@gmail.com>
2024-02-01 13:48:52 +0100

    fix api consuming

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,13 +1,33 @@
+import {useState, useEffect} from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
+import { getAllProducts } from '../../utils/api';
 
 function Home() {
+
+  const [items, setItems]= useState([]);
+
+  useEffect(()=> {
+    getAllProducts().then(res => {
+      console.log(res)
+      setItems(res)
+    });
+  }, [])
 
     return (
       
       <Layout>
         Home
-        <Card/>
+        <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg '>
+        {
+          items.map((item)=>{
+            return <Card key={item.id} item={item}  />
+          })
+        }
+
+        </div>
+        
+        
       </Layout>
       
       
