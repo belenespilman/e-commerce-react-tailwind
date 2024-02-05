43d8d5c90154a45be77bebeb17a785a7dc3fe31f
belenespilman <belen.espilman@gmail.com>
2024-02-05 15:33:50 +0100

    showing products in product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,16 +4,28 @@ export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({children}) => {
 
+
+    //Shopping Cart - Increment amount
     const [count, setCount]= useState(0);
+
+    //Product Detail - Open - Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
-    const closeProductDetail = () => {}
-    const openProductDetail= () => {
-        setIsProductDetailOpen(true)
-    }
+    const closeProductDetail = () => setIsProductDetailOpen(false)
+    const openProductDetail= () => setIsProductDetailOpen(true)
+        
+    //Product Detail - Show product
+    const [productToShow, setProductToShow] = useState({});
 
     return (
-        <ShoppingCartContext.Provider value={{count, setCount, openProductDetail,
-        closeProductDetail, isProductDetailOpen }}>
+        <ShoppingCartContext.Provider value={{
+        count,
+        setCount, 
+        openProductDetail,
+        closeProductDetail, 
+        isProductDetailOpen,
+        productToShow,
+        setProductToShow }}>
+
         {children}
         </ShoppingCartContext.Provider>
     )
